a4e8f6ecb34fcf07f727ec836e3d105285edd6e3
BluediamondPVT <websolutionsbdit@gmail.com>
2025-10-02 18:45:46 +0530

    install library

---
+++ src/components/toggle.tsx
@@ -0,0 +1,9 @@
+import { ThemeToggleButton } from "@/components/ui/skiper-ui/skiper26";
+
+const Toggle = () => (
+  <div className="flex items-center justify-center p-8">
+    <ThemeToggleButton variant="circle" start="center" />
+  </div>
+);
+
+export default Toggle;
